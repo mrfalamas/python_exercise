78b792849f8ce175b0cd2d4830486737cd0ce4b5
Ovidiu Falamas <extern.ovidiu.falamas@cariad.technology>
2025-01-22 11:54:10 +0200

    added diff function

--- implementation.py
+++ implementation.py
@@ -9,8 +9,13 @@ def suma(a, b):
     print('Suma este: ' + str(sum))
     return sum
 
+def sub(a, b):
+    return abs(a - b)
+
 def main():
     #call function in main
     suma(ovi, darius)
-    
+    diff = sub(ovi, darius)
+    print('Diff is: ' + str(diff))
+     
 if __name__ == "__main__": main()    
