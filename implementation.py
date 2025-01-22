29e2df1b7323c54f321f189516e04e97cd073eda
Ovidiu Falamas <extern.ovidiu.falamas@cariad.technology>
2025-01-22 10:29:22 +0200

    update aexcel and aded main to py

--- implementation.py
+++ implementation.py
@@ -3,3 +3,14 @@ print('hello world')
 ovi = 10
 
 darius = 11
+
+def suma(a, b):
+    sum = a + b
+    print('Suma este: ' + str(sum))
+    return sum
+
+def main():
+    #call function in main
+    suma(ovi, darius)
+    
+if __name__ == "__main__": main()    
