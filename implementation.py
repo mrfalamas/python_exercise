8cc7e42f40f5a35552146938991ac1a90109072f
Ovidiu Falamas <extern.ovidiu.falamas@cariad.technology>
2025-01-23 13:09:13 +0200

    facut curat in py

--- implementation.py
+++ implementation.py
@@ -1,38 +1,5 @@
-print('hello world')
-
-ovi = 10
-
-darius = 11
-
-def suma(a, b):
-    sum = a + b
-    print('Suma este: ' + str(sum))
-    return sum
-
-def sub(a, b):
-    return abs(a - b)
-
-def produs(a,b):
-    prod = a * b
-    print('Produsul este: ' + str(prod))
-    return prod
-
-def impartire(a,b):
-    if b != 0:
-        rez = a / b
-        print('Impartirea este: ' + str(rez))
-        return rez
-    else:
-        print('Eroare: Impartirea la 0 nu este permisa')
-
 def main():
-    #call function in main
-    produs(ovi, darius)
-    suma(ovi, darius)
-    impartire(ovi, darius)
-
-    diff = sub(ovi, darius)
-    print('Diff is: ' + str(diff))
+    print('hello world')
      
 if __name__ == "__main__": 
     main()    
