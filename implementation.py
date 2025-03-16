3c0009854fb06baa349be3ce2c7b73bbc933eb6a
Falamas Ovidiu <mrfalamas@gmail.com>
2025-03-16 20:58:45 +0200

    update info

--- implementation.py
+++ implementation.py
@@ -1,6 +1,11 @@
+"""Module providing a function printing python version."""
+
+
 def main():
+    """Function printing python version."""
     print('hello world')
     print('other thing')
-     
-if __name__ == "__main__": 
-    main()    
+
+
+if __name__ == "__main__":
+    main()
