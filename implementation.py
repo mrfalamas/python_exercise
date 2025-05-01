91dbc648210d74221401844dea3b6e06469c7e74
Falamas Ovidiu <mrfalamas@gmail.com>
2025-05-01 11:57:10 +0300

    added proj

--- implementation.py
+++ implementation.py
@@ -10,8 +10,8 @@ import os.path
 
 main_dict = {}
 
-def append_from_json(gr_dir):
-    json_file = os.path.join(gr_dir, "01_review_test_spec.json")
+def append_from_json(gr_dir, project):
+    json_file = os.path.join(gr_dir, project + "_01_review_test_spec.json")
     with open(json_file, 'r') as file:
         data = json.load(file)
 
@@ -20,8 +20,8 @@ def append_from_json(gr_dir):
         if module not in main_dict.keys():
             main_dict[module] = data[key]
 
-def append_from_txt(gr_dir):
-    txt_file = os.path.join(gr_dir, "02_review_test_impl.txt")
+def append_from_txt(gr_dir, project):
+    txt_file = os.path.join(gr_dir, project + "_02_review_test_impl.txt")
     f = open(txt_file, "r")
     for line in f:
         module = line.split(':')[0]
@@ -34,8 +34,8 @@ def append_from_txt(gr_dir):
         for res in split_res:
             main_dict[module].append(int(res))
 
-def append_from_xml(gr_dir):
-    xml_file = os.path.join(gr_dir, "03_nonsafety_test.xml")
+def append_from_xml(gr_dir, project):
+    xml_file = os.path.join(gr_dir, project + "_03_nonsafety_test.xml")
     tree = ET.parse(xml_file)
     modules = tree.findall("module")
 
@@ -51,8 +51,8 @@ def append_from_xml(gr_dir):
         for res in each_res:
             main_dict[mod_name].append(int(res))
 
-def append_from_xls(gr_dir):
-    xls_file = os.path.join(gr_dir, "04_safety_test.xlsx")
+def append_from_xls(gr_dir, project):
+    xls_file = os.path.join(gr_dir, project + "_04_safety_test.xlsx")
     data = pd.read_excel(xls_file, header=None, index_col=None)
 
     for row in data.iterrows():
@@ -65,7 +65,7 @@ def append_from_xls(gr_dir):
                 if not(math.isnan(elem)):
                     main_dict[person].append(int(elem))
 
-def create_html(gr_dir, type):
+def create_html(gr_dir, project, type):
     html_file = os.path.join(gr_dir, "output.html")
     file = open(html_file, "w")
 
@@ -78,7 +78,7 @@ def create_html(gr_dir, type):
             mod_fails = html_dict[module][2]
             open_rate = mod_opens*100 / mod_checks
             fail_rate = mod_fails*100 / mod_checks
-            status = "NOK" if open_rate > 0 or fail_rate > 0 else "OK"
+            status = "NOK" if fail_rate > 0 else "OPEN" if open_rate > 0 else "OK"
             html_dict[module].append(f'{open_rate:.2f}' + "%")
             html_dict[module].append(f'{fail_rate:.2f}' + "%")
             html_dict[module].append(status)
@@ -94,7 +94,7 @@ def create_html(gr_dir, type):
             req_fails = html_dict[module][5]
             req_op_rate = req_opens*100 / req_total
             req_fl_rate = req_fails*100 / req_total
-            status = "NOK" if open_rate > 0 or fail_rate > 0 or req_op_rate > 0 or req_fl_rate > 0 else "OK"
+            status = status = "NOK" if (fail_rate > 0 or req_fl_rate > 0) else "OPEN" if (open_rate > 0 or req_op_rate > 0) else "OK"
             html_dict[module].append(f'{open_rate:.2f}' + "%")
             html_dict[module].append(f'{fail_rate:.2f}' + "%")
             html_dict[module].append(f'{req_op_rate:.2f}' + "%")
@@ -107,23 +107,28 @@ def create_html(gr_dir, type):
     file.write("<title>Test Results</title>")
     file.write("</head>")
     file.write("<body>")
+    file.write("<h1><span style=\"font-size: 15px;\">&#128310; PROJECT&nbsp;&nbsp;</span><span style=\"color: #f8641a; font-size: 35px;\">" + project + "</span></h1>")
+    file.write("<p> </p>")
     if "01" in type:
-        file.write("<h1>Review of Test Specification</h1>")
+        file.write("<h2><span style=\"font-size: 14px;\">&#128311; ACTIVITY&nbsp;&nbsp;&nbsp;</span><span style=\"color: #0078c4; font-size: 25px;\">Review of Test Specification</span></h2>")
         file.write("<p> </p>")
-        file.write("<h3>&rArr; readed from <span style=\"color: #d35400; font-size: 25px;\">01_test_specification.JSON</span></h3>")
+        file.write("<p>&#128312; html report generated at <span style=\"color: #f8641a; font-size: 18px;\">" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "</span></h3>")
+        file.write("<p>&#128313; readed from <span style=\"color: #0078c4; font-size: 18px;\">01_test_specification.JSON</span></p>")
     if "02" in type:
-        file.write("<h1>Review of Test Implementation</h1>")
+        file.write("<h2><span style=\"font-size: 14px;\">&#128311; ACTIVITY&nbsp;&nbsp;&nbsp;</span><span style=\"color: #0078c4; font-size: 25px;\">Review of Test Implementation</span></h2>")
         file.write("<p> </p>")
-        file.write("<h3>&rArr; readed from <span style=\"color: #d35400; font-size: 25px;\">02_test_implementation.TXT</span></h3>")
+        file.write("<p>&#128312; html report generated at <span style=\"color: #f8641a; font-size: 18px;\">" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "</span></h3>")
+        file.write("<p>&#128313; readed from <span style=\"color: #0078c4; font-size: 18px;\">02_test_implementation.TXT</span></p>")
     if "03" in type:
-        file.write("<h1>Non-Safety Tests (QM Requirements)</h1>")
+        file.write("<h2><span style=\"font-size: 14px;\">&#128311; ACTIVITY&nbsp;&nbsp;&nbsp;</span><span style=\"color: #0078c4; font-size: 25px;\">Non-Safety Tests (QM Requirements)</span></h2>")
         file.write("<p> </p>")
-        file.write("<h3>&rArr; readed from <span style=\"color: #d35400; font-size: 25px;\">03_test_qm_requirements.XML</span></h3>")
+        file.write("<p>&#128312; html report generated at <span style=\"color: #f8641a; font-size: 18px;\">" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "</span></h3>")
+        file.write("<p>&#128313; readed from <span style=\"color: #0078c4; font-size: 18px;\">03_test_qm_requirements.XML</span></p>")
     if "04" in type:
-        file.write("<h1>Safety Tests (ASIL Requirements)</h1>")
+        file.write("<h2><span style=\"font-size: 14px;\">&#128311; ACTIVITY&nbsp;&nbsp;&nbsp;</span><span style=\"color: #0078c4; font-size: 25px;\">Safety Tests (ASIL Requirements)</span></h2>")
         file.write("<p> </p>")
-        file.write("<h3>&rArr; readed from <span style=\"color: #d35400; font-size: 25px;\">04_test_asil_requirements.XLSX</span></h3>")
-    file.write("<h3>&rArr; html report generated at <span style=\"color: #148f77; font-size: 22px;\">" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "</span></h3>")
+        file.write("<p>&#128312; html report generated at <span style=\"color: #f8641a; font-size: 18px;\">" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "</span></h3>")
+        file.write("<p>&#128313; readed from <span style=\"color: #0078c4; font-size: 18px;\">04_test_asil_requirements.XLSX</span></p>")
     file.write("<p> </p>")
     file.write("<table style=\"border:1px solid black; border-collapse: collapse; \">")
     file.write("<tr style=\"border:1px solid black; \">")
@@ -158,9 +163,11 @@ def create_html(gr_dir, type):
                     file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; background-color: #fadbd8 ;\">" + str(value) + "</td>")
                 elif i == 5:
                     if str(value) == "OK":
-                        file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; color: green;\">" + str(value) + "</td>")
+                        file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center;\">&#9989;</td>")
+                    elif str(value) == "OPEN":
+                        file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center;\">&#128260;</td>")
                     else:
-                        file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; color: red;\">" + str(value) + "</td>")
+                        file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; font-size: 14px;\">&#10060;</td>")
                 else:
                     file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center;\">" + str(value) + "</td>")
             file.write("</tr>")
@@ -181,9 +188,11 @@ def create_html(gr_dir, type):
             file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; background-color: #85c1e9 ;\">" + str(html_dict[row][8]) + "</td>")
             file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; background-color: #f1948a ;\">" + str(html_dict[row][9]) + "</td>")
             if str(html_dict[row][10]) == "OK":
-                file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; color: green;\">" + str(html_dict[row][10]) + "</td>")
+                file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center;\">&#9989;</td>")
+            elif str(html_dict[row][10]) == "OPEN":
+                file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center;\">&#128260;</td>")
             else:
-                file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; color: red;\">" + str(html_dict[row][10]) + "</td>")
+                file.write("<td style=\"border:1px solid black; padding: 10px; text-align: center; font-size: 14px;\">&#10060;</td>")
             file.write("</tr>")
     file.write("</table>")
     file.write("</body>")
@@ -194,29 +203,29 @@ def create_html(gr_dir, type):
 
     print("HTML file successfully written.")
 
-def create_report(type):
+def create_report(project, type):
     py_dir = os.path.dirname(os.path.abspath(__file__))
     gr_dir = os.path.join(py_dir, "test_results")
 
     if "01" in type:
-        append_from_json(gr_dir)
+        append_from_json(gr_dir, project)
     if "02" in type:
-        append_from_txt(gr_dir)
+        append_from_txt(gr_dir, project)
     if "03" in type:
-        append_from_xml(gr_dir)
+        append_from_xml(gr_dir, project)
     if "04" in type:
-        append_from_xls(gr_dir)
+        append_from_xls(gr_dir, project)
 
-    create_html(gr_dir, type)
+    create_html(gr_dir, project, type)
 
 def main():
     arg = sys.argv
-    if arg[1] == 'none':
+    if arg[1] == 'none' or arg[2] == 'none':
         print("please select a valid option from build parameters")
     else:
-        print("calling python script with: " + arg[1])
+        print("calling python script with project: " + arg[1] + " and test activity: " + arg[2])
 
-    create_report(arg[1])
+    create_report(arg[1], arg[2])
 
 if __name__ == "__main__":
     main()
