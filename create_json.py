49ce6c1b8a6f1f7a89d3808499c4ef269eb80259
Ovidiu Falamas <extern.ovidiu.falamas@cariad.technology>
2025-05-06 13:01:34 +0300

    aupdate

--- create_json.py
+++ create_json.py
@@ -1,12 +1,13 @@
 """Python implementation for creating a json dictionary"""
 import os
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import json
 import sys
 import pandas as pd
 import math
 
-json_dict = {}
+activity_dict = {}
+module_dict = {}
 py_dir = os.path.dirname(os.path.abspath(__file__))
 tst_dir = os.path.join(py_dir, "test_results")
 html_path = os.path.join(tst_dir, "output.html")
@@ -51,18 +52,41 @@ def from_excel():
     if os.path.isfile(tpsr_path):
         data = pd.read_excel(tpsr_path, header=None, index_col=None)
 
-    for row in data.iterrows():
-        for i, elem in enumerate(row[1]):
-            if i == 0:
-                person = elem
-                if person not in json_dict.keys():
-                    json_dict[person] = []
-            else:
-                if not(math.isnan(elem)):
-                    json_dict[person].append(int(elem))
-
+        for j, row in enumerate(data.iterrows()):
+            if j == 0: 
+                for i, elem in enumerate(row[1]):
+                    if i in [1, 7, 13, 24]:
+                        if elem not in activity_dict.keys():
+                            activity_dict[elem] = {}   
+            if j > 1: 
+                for i, elem in enumerate(row[1]):
+                    module_name = row[1][0]
+                    if (i >= 1 and i < 7):                        
+                        if module_name not in activity_dict["Review of Test Specification"].keys():
+                            activity_dict["Review of Test Specification"][module_name] = []
+                        else:    
+                            activity_dict["Review of Test Specification"][module_name].append(str(elem))                      
+                    if (i >= 7 and i < 13):
+                        if module_name not in activity_dict["Review of Test Implementation"].keys():
+                            activity_dict["Review of Test Implementation"][module_name] = []
+                        else:  
+                            activity_dict["Review of Test Implementation"][module_name].append(str(elem))  
+                    if (i >= 13 and i < 24):
+                        if module_name not in activity_dict["Non-Safety Tests (QM Requirements)"].keys():
+                            activity_dict["Non-Safety Tests (QM Requirements)"][module_name] = []
+                        else:  
+                            activity_dict["Non-Safety Tests (QM Requirements)"][module_name].append(str(elem))  
+                    if (i >= 24 and i < 35):
+                        if module_name not in activity_dict["Safety Tests (ASIL Requirements)"].keys():
+                            activity_dict["Safety Tests (ASIL Requirements)"][module_name] = []
+                        else:  
+                            activity_dict["Safety Tests (ASIL Requirements)"][module_name].append(str(elem))  
+                                          
+        print(activity_dict)
+        print(module_dict)
+        
         r = open(json_path, 'wb')
-        json_string = json.dumps(json_dict, indent=len(json_dict))
+        json_string = json.dumps(activity_dict, indent=len(activity_dict))
         r.write(json_string.encode('utf-8'))
         if os.path.isfile(json_path):
             print('File output.json was created')
